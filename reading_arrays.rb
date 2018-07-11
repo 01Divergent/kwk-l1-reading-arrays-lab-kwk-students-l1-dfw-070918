e93bc2571f610d07a5135df26fecac7752c39660
01Divergent <iphone.claudia@icloud.com>
2018-07-11 14:59:47 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -8,25 +8,73 @@ student_names = [
 ]
 
 def first_student_by_index
+  student_names = [
+  "Adele",
+  "Beyoncé",
+  "Cardi B",
+  "Lady Gaga",
+  "Nicki Minaj",
+  "Rihanna"
+]
   puts student_names[0]
 end
 
 def fourth_student_by_index
+  student_names = [
+  "Adele",
+  "Beyoncé",
+  "Cardi B",
+  "Lady Gaga",
+  "Nicki Minaj",
+  "Rihanna"
+]
   puts student_names[3]
 end
 
 def last_student_by_index
+  student_names = [
+  "Adele",
+  "Beyoncé",
+  "Cardi B",
+  "Lady Gaga",
+  "Nicki Minaj",
+  "Rihanna"
+]
   puts student_names[5]
 end
 
 def first_student_by_method
+  student_names = [
+  "Adele",
+  "Beyoncé",
+  "Cardi B",
+  "Lady Gaga",
+  "Nicki Minaj",
+  "Rihanna"
+]
   puts student_names.first
 end
 
 def last_student_by_method
+  student_names = [
+  "Adele",
+  "Beyoncé",
+  "Cardi B",
+  "Lady Gaga",
+  "Nicki Minaj",
+  "Rihanna"
+]
   puts student_names.last
 end
 
 def first_second_and_third_students
+  student_names = [
+  "Adele",
+  "Beyoncé",
+  "Cardi B",
+  "Lady Gaga",
+  "Nicki Minaj",
+  "Rihanna"
+]
   puts student_names[0,1,2]
 end
