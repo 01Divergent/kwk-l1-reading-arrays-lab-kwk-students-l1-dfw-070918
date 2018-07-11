29663e80d01b1df3b7ed900b830bd55d7fd81d3d
01Divergent <iphone.claudia@icloud.com>
2018-07-11 15:08:31 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -8,38 +8,31 @@ STUDENT_NAMES = [
 ]
 
 def first_student_by_index
-  student_names = ["Adele","Beyoncé","Cardi B","Lady Gaga","Nicki Minaj","Rihanna"]
+  STUDENT_NAMES = ["Adele","Beyoncé","Cardi B","Lady Gaga","Nicki Minaj","Rihanna"]
   return STUDENT_NAMES[0]
 end
 
 def fourth_student_by_index
-  student_names = ["Adele","Beyoncé","Cardi B","Lady Gaga","Nicki Minaj","Rihanna"]
-  return student_names[3]
+  STUDENT_NAMES = ["Adele","Beyoncé","Cardi B","Lady Gaga","Nicki Minaj","Rihanna"]
+  return STUDENT_NAMES[3]
 end
 
 def last_student_by_index
-  student_names = ["Adele","Beyoncé","Cardi B","Lady Gaga","Nicki Minaj","Rihanna"]
-  return student_names[5]
+  STUDENT_NAMES = ["Adele","Beyoncé","Cardi B","Lady Gaga","Nicki Minaj","Rihanna"]
+  return STUDENT_NAMES[5]
 end
 
 def first_student_by_method
-  student_names = ["Adele","Beyoncé","Cardi B","Lady Gaga","Nicki Minaj","Rihanna"]
-  return student_names.first
+  STUDENT_NAMES = ["Adele","Beyoncé","Cardi B","Lady Gaga","Nicki Minaj","Rihanna"]
+  return STUDENT_NAMES.first
 end
 
 def last_student_by_method
-  student_names = ["Adele","Beyoncé","Cardi B","Lady Gaga","Nicki Minaj","Rihanna"]
-  return student_names.last
+  STUDENT_NAMES = ["Adele","Beyoncé","Cardi B","Lady Gaga","Nicki Minaj","Rihanna"]
+  return STUDENT_NAMES.last
 end
 
 def first_second_and_third_students
-  student_names = ["Adele","Beyoncé","Cardi B","Lady Gaga","Nicki Minaj","Rihanna"]
-  return student_names[0,1,2]
+  STUDENT_NAMES = ["Adele","Beyoncé","Cardi B","Lady Gaga","Nicki Minaj","Rihanna"]
+  return STUDENT_NAMES[0,1,2]
 end
-
-first_student_by_index
-fourth_student_by_index
-last_student_by_index
-first_student_by_method
-last_student_by_method
-first_second_and_third_students
